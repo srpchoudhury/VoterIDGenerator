3168429288931b9f7b6805a6df31d230c11eca42
S Rudra Prasad Choudhury <98145990+srpchoudhury@users.noreply.github.com>
2022-11-08 20:17:39 +0530

    VotedIDGenerator main class

---
+++ github.java
@@ -0,0 +1,71 @@
+//AgeLimitException
+class AgeLimitException extends Exception{
+
+    AgeLimitException(){
+        super();
+    }
+    AgeLimitException(String msg){
+        super(msg);
+    }
+}
+
+// Voter.java
+public class Voter {
+    private int age;
+    private String name;
+
+    public void setAge(int age)
+            throws AgeLimitException {
+        if (age < 18 || age > 120)
+            throw new AgeLimitException("You are not eligible for voting");
+        this.age = age;
+
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
+
+// VoterRegOffice
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class VoterRegOffice {
+    public static void main(String... args) {
+        Scanner scn = new Scanner(System.in);
+
+        while (true) {
+            Voter v = new Voter();
+
+            try {
+                System.out.print("\nEnter Name: ");
+                v.setName(scn.nextLine());
+
+                System.out.print("Enter age: ");
+                v.setAge(scn.nextInt());
+
+                System.out.println("Hi " + v.getName() + " your details are saved");
+                System.out.println("Please collect your voter card");
+                System.out.println("from ESEVE after 30 days");
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Please pass only integer");
+            } catch (AgeLimitException e) {
+                System.out.println(e.getMessage());
+            }
+            scn.nextLine();
+
+        }
+
+    }
+
+}/* Thank you........ */
